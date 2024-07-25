95677f2883d9c50a449ef4b6cb91866ed5fdf06c
olah <olahisrael07@gmail.com>
2024-07-25 21:14:10 +0100

    Added Map feature

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,9 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
+
+const {searchParams, setSearchParams} = useSearchParams();
+const lat = searchParams.get("lat");
+const lng = searchParams.get("lng");
 
 function Map() {
     return (
