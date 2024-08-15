59fd24bc9ac2467126663ce99ff89d2375776bc1
olah <olahisrael07@gmail.com>
2024-08-15 10:01:00 +0100

    Added spinner to full page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import { lazy, Suspense } from "react";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+// import Login from "./pages/Login";
+
+// dist/assets/index-hhcpLjOE.css   29.95 kB │ gzip:   5.05 kB
+// dist/assets/index-BVXjkqvt.js   508.58 kB │ gzip: 148.64 kB
+
+const Homepage = lazy(()=> import("./pages/Homepage"));
+const Pricing = lazy(()=> import("./pages/Pricing"));
+const Product = lazy(()=> import("./pages/Product"));
+const AppLayout = lazy(()=> import("./pages/AppLayout"));
+const PageNotFound = lazy(()=> import("./pages/PageNotFound"));
+const Login = lazy(()=> import("./pages/Login"));
+
+
+
+
 
 
 function App() {
@@ -19,6 +38,7 @@ function App() {
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage />}>
           <Routes>
             <Route path="/" element={<Homepage />}/>
             <Route path="product" element={<Product />}/>
@@ -38,6 +58,7 @@ function App() {
             </Route>
             <Route path="*" element={<PageNotFound />}/>
           </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
